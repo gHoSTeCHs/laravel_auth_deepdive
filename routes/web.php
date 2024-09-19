e4d3e23ba8ccaa81273d2324f33c1b159973d92a
Somadina Amuchie <kingswatter@gmail.com>
2024-09-19 15:34:08 +0100

    scffolding, design, and route decleration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,4 @@ Route::get('/', function () {
 });
 
 Route::get('/register', [RegisteredUserController::class, 'index']);
+Route::get('/login', [SessionController::class, 'index']);
